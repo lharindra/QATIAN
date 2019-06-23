39a36e6117750cedb5879137378c6fb4b3c264b9
root <root@ip-172-31-3-232.us-east-2.compute.internal>
2019-06-23 19:44:01 +0000

    updated with QAT folder

--- varlog.sh
+++ varlog.sh
@@ -1,70 +1,70 @@
 #!/bin/bash
-awk -v d1="$(date --date="-1440 min" "+%b %_d %H:%M")" -v d2="$(date "+%b %_d %H:%M")" '$0 > d1 && $0 < d2 || $0 ~ d2' /var/log/messages | grep -i error > /tmp/error.log
-if [[ $(wc -l < /tmp/error.log) -eq 0 ]]
+awk -v d1="$(date --date="-1440 min" "+%b %_d %H:%M")" -v d2="$(date "+%b %_d %H:%M")" '$0 > d1 && $0 < d2 || $0 ~ d2' /var/log/messages | grep -i error > /tmp/QAT/error.log
+if [[ $(wc -l < /tmp/QAT/error.log) -eq 0 ]]
 then
  echo -e "No ERORS found under /var/log/messages" #please don't edit the typo ERORS(to exclude from the fileter we made a typo)
- rm -rf /tmp/error.log
+ rm -rf /tmp/QAT/error.log
 else
- echo -e "ERROR:- Still there are some ERROR logs under /var/log/messages!!!! Please go and fix them ---- find the in file /tmp/error.log"
+ echo -e "ERROR:- Still there are some ERROR logs under /var/log/messages!!!! Please go and fix them ---- find the in file /tmp/QAT/error.log"
 fi
-awk -v d1="$(date --date="-1440 min" "+%b %_d %H:%M")" -v d2="$(date "+%b %_d %H:%M")" '$0 > d1 && $0 < d2 || $0 ~ d2' /var/log/messages | grep -i warning > /tmp/warning.log
-if [[ $(wc -l < /tmp/warning.log) -eq 0 ]]
+awk -v d1="$(date --date="-1440 min" "+%b %_d %H:%M")" -v d2="$(date "+%b %_d %H:%M")" '$0 > d1 && $0 < d2 || $0 ~ d2' /var/log/messages | grep -i warning > /tmp/QAT/warning.log
+if [[ $(wc -l < /tmp/QAT/warning.log) -eq 0 ]]
 then
  echo -e "No WARNINGS found under /var/log/messages"
- rm -rf /tmp/warning.log
+ rm -rf /tmp/QAT/warning.log
 else
- echo -e "ERROR:- Still there are some WARNINGS logs under /var/log/messages!!!! Please go and fix them ---- find the in file /tmp/warning.log"
+ echo -e "ERROR:- Still there are some WARNINGS logs under /var/log/messages!!!! Please go and fix them ---- find the in file /tmp/QAT/warning.log"
 fi
-awk -v d1="$(date --date="-1440 min" "+%b %_d %H:%M")" -v d2="$(date "+%b %_d %H:%M")" '$0 > d1 && $0 < d2 || $0 ~ d2' /var/log/messages | grep -i failed > /tmp/failed.log
-if [[ $(wc -l < /tmp/failed.log) -eq 0 ]]
+awk -v d1="$(date --date="-1440 min" "+%b %_d %H:%M")" -v d2="$(date "+%b %_d %H:%M")" '$0 > d1 && $0 < d2 || $0 ~ d2' /var/log/messages | grep -i failed > /tmp/QAT/failed.log
+if [[ $(wc -l < /tmp/QAT/failed.log) -eq 0 ]]
 then
  echo -e "No FAILED found under /var/log/messages"
- rm -rf /tmp/failed.log
+ rm -rf /tmp/QAT/failed.log
 else
- echo -e "ERROR:- Still there are some FAIL logs under /var/log/messages!!!! Please go and fix them ---- find the in file /tmp/failed.log"
+ echo -e "ERROR:- Still there are some FAIL logs under /var/log/messages!!!! Please go and fix them ---- find the in file /tmp/QAT/failed.log"
 fi
-awk -v d1="$(date --date="-1440 min" "+%b %_d %H:%M")" -v d2="$(date "+%b %_d %H:%M")" '$0 > d1 && $0 < d2 || $0 ~ d2' /var/log/messages | grep -i critical > /tmp/critical.log
-if [[ $(wc -l < /tmp/critical.log) -eq 0 ]]
+awk -v d1="$(date --date="-1440 min" "+%b %_d %H:%M")" -v d2="$(date "+%b %_d %H:%M")" '$0 > d1 && $0 < d2 || $0 ~ d2' /var/log/messages | grep -i critical > /tmp/QAT/critical.log
+if [[ $(wc -l < /tmp/QAT/critical.log) -eq 0 ]]
 then
  echo -e "No ERRORS found under /var/log/messages"
- rm -rf /tmp/critical.log
+ rm -rf /tmp/QAT/critical.log
 else
- echo -e "ERROR:- Still there are some CRITICAL logs under /var/log/messages!!!! Please go and fix them ---- find the in file /tmp/critical.log"
+ echo -e "ERROR:- Still there are some CRITICAL logs under /var/log/messages!!!! Please go and fix them ---- find the in file /tmp/QAT/critical.log"
 fi
-if [[ -f /tmp/varlog.log ]]
+if [[ -f /tmp/QAT/varlog.log ]]
 then
- cp /dev/null /tmp/varlog.log
- echo -e "This was created on [$(date)]" >> /tmp/varlog.log
+ cp /dev/null /tmp/QAT/varlog.log
+ echo -e "This was created on [$(date)]" >> /tmp/QAT/varlog.log
 fi
-if [[ -f /tmp/error.log ]]
+if [[ -f /tmp/QAT/error.log ]]
 then
- echo -e "++++++++++++++" >> /tmp/varlog.log
- echo -e "X ERROR LOGS X" >> /tmp/varlog.log
- echo -e "++++++++++++++" >> /tmp/varlog.log
- cat /tmp/error.log >> /tmp/varlog.log
- rm -rf /tmp/error.log
+ echo -e "++++++++++++++" >> /tmp/QAT/varlog.log
+ echo -e "X ERROR LOGS X" >> /tmp/QAT/varlog.log
+ echo -e "++++++++++++++" >> /tmp/QAT/varlog.log
+ cat /tmp/QAT/error.log >> /tmp/QAT/varlog.log
+ rm -rf /tmp/QAT/error.log
 fi
-if [[ -f /tmp/warning.log ]]
+if [[ -f /tmp/QAT/warning.log ]]
 then 
- echo -e "++++++++++++++++" >> /tmp/varlog.log
- echo -e "X WARNING LOGS X" >> /tmp/varlog.log
- echo -e "++++++++++++++++" >> /tmp/varlog.log
- cat /tmp/warning.log >> /tmp/varlog.log
- rm -rf /tmp/warning.log
+ echo -e "++++++++++++++++" >> /tmp/QAT/varlog.log
+ echo -e "X WARNING LOGS X" >> /tmp/QAT/varlog.log
+ echo -e "++++++++++++++++" >> /tmp/QAT/varlog.log
+ cat /tmp/QAT/warning.log >> /tmp/QAT/varlog.log
+ rm -rf /tmp/QAT/warning.log
 fi
-if [[ -f /tmp/failed.log ]]
+if [[ -f /tmp/QAT/failed.log ]]
 then 
- echo -e "+++++++++++++" >> /tmp/varlog.log
- echo -e "X FAIL LOGS X" >> /tmp/varlog.log
- echo -e "+++++++++++++" >> /tmp/varlog.log
- cat /tmp/failed.log >> /tmp/varlog.log
- rm -rf /tmp/failed.log
+ echo -e "+++++++++++++" >> /tmp/QAT/varlog.log
+ echo -e "X FAIL LOGS X" >> /tmp/QAT/varlog.log
+ echo -e "+++++++++++++" >> /tmp/QAT/varlog.log
+ cat /tmp/QAT/failed.log >> /tmp/QAT/varlog.log
+ rm -rf /tmp/QAT/failed.log
 fi
-if [[ -f /tmp/critical.log ]]
+if [[ -f /tmp/QAT/critical.log ]]
 then
- echo -e "+++++++++++++++++" >> /tmp/varlog.log
- echo -e "X CRITICAL LOGS X" >> /tmp/varlog.log
- echo -e "+++++++++++++++++" >> /tmp/varlog.log
- cat /tmp/critical.log >> /tmp/varlog.log
- rm -rf /tmp/critical.log
+ echo -e "+++++++++++++++++" >> /tmp/QAT/varlog.log
+ echo -e "X CRITICAL LOGS X" >> /tmp/QAT/varlog.log
+ echo -e "+++++++++++++++++" >> /tmp/QAT/varlog.log
+ cat /tmp/QAT/critical.log >> /tmp/QAT/varlog.log
+ rm -rf /tmp/QAT/critical.log
 fi
