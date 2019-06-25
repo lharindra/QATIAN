3e7a071cd86f5751efef6b9f79116097e3b5a7b3
lharindra <43954033+lharindra@users.noreply.github.com>
2019-06-25 15:43:49 +0530

    Update qatesting.sh

--- qatesting.sh
+++ qatesting.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo su -
 mkdir -p /tmp/QAT
 echo -e "+++++++++++++++++++++++++++++++++++++++"
 echo -e "X QAT TESTING GOING TO BE STARTED !!! X"
@@ -231,20 +232,7 @@ echo -e "-------------------------------------------"
 yum check-update > /tmp/QAT/Yum_${hostname}
 if [[ $? -eq 100 ]]
 then
- echo "System has Security system patches. Please go ahead and patch them(if required)"
- sleep 2
- while true; do
-    read -p "Do you wish to install all the available patches? Please give the input(y/n):-" yn
-    if [[ "$yn" == "y" || "$yn" == "Y" ]]
-    then
-     read -p "Re-Confirm please type(y/n):-" yn
-    fi
-    case $yn in
-        [Yy]* ) yum update -y 2> /dev/null ; if [[ $? -eq 0 ]]; then echo -e "Patching is completed successfully"; else echo -e "ERROR:- YUM has some issues while patching"; fi; break;;
-        [Nn]* ) echo -e "As you had selected (No) it's quiting!!!"; echo -e "ERROR:- Updates are available on the host"; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
- done
+ echo "ERROR:- System has Security system patches. Please go ahead and patch them(if required)"
 elif [[ $? -eq 0 ]]
 then
  echo -e "Great stuff!! No patches to install"
