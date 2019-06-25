1bc8ca9ba8fd5dc0264166341be04ee6028891e2
lharindra <43954033+lharindra@users.noreply.github.com>
2019-06-25 13:32:00 +0530

    Update result.sh

--- result.sh
+++ result.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cat /tmp/QAT/$host.log | grep -i error | awk '{$1=""; print}' > /tmp/QAT/$host_defects
-if [[ $(wc -l < /tmp/QAT/$host_defects) -eq 0 ]]
+cat /tmp/QAT/${host}.log | grep -i error | awk '{$1=""; print}' > /tmp/QAT/${host}_defects
+if [[ $(wc -l < /tmp/QAT/${host}_defects) -eq 0 ]]
 then
  echo -e "Great Stuff :) !!! not defects found on the host ${host}"
 else
- echo -e "$(wc -l < /tmp/QAT/$host_defects) defects are found while testing the host ${host} and they are......"
- cat -n /tmp/QAT/$host_defects
+ echo -e "$(wc -l < /tmp/QAT/${host}_defects) defects are found while testing the host ${host} and they are......"
+ cat -n /tmp/QAT/${host}_defects
 fi
